705e83f2a52e6be23e324a9a35196a48a780bf1f
Aiden-BachDuong-ICD201-01 <aiden.bachduong@student.tdsb.on.ca>
2024-09-17 14:15:17 +0000

    work

--- PP1_3.py
+++ PP1_3.py
@@ -8,12 +8,12 @@
 def q1():
   #Write Assignment code here
   #Remember your code must be indented once
-  print (" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n\n* * * * *\n*       *\n*       *\n*       *\n* * * * * \n\n* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *\n")
+  print (" * * * * *\n* * * * *\n * * * * *\n* * * * *\n * * * * *\n* * * * *\n\n* * * *\n*     *\n*     *\n*     *\n* * * * \n\n* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *\n")
   
 
 def q2():
   #Write Assignment code here
-  print('"Hello \nWorld"\n')
+  print('"Hello "\n"World"\n')
   
 def q3():
   #Write Assignment code here
